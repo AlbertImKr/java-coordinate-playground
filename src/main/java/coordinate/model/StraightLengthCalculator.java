b9b79ec8be0a7b0659dad08b80eca3d4a6d82835
Albert <guang7226961@naver.com>
2022-11-05 09:56:51 +0900

    feat($model) 직선길이구하기(StraightLengthCalculator) 두점 사이 거리를 리포트한다.

--- src/main/java/coordinate/model/StraightLengthCalculator.java
+++ src/main/java/coordinate/model/StraightLengthCalculator.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 class StraightLengthCalculator {
+    private final List<CoordinatePoint> coordinatePoints;
+
     public StraightLengthCalculator(List<CoordinatePoint> coordinatePoints) {
         validate(coordinatePoints);
-
+        this.coordinatePoints = coordinatePoints;
     }
 
     private void validate(List<CoordinatePoint> coordinatePoints) {
@@ -14,5 +16,11 @@ class StraightLengthCalculator {
         if (!distinctCoordinatePoints.equals(coordinatePoints)) {
             throw new IllegalArgumentException("두 좌표가 서로 달아랴 한다.");
         }
+    }
+
+    public double reportResult() {
+        CoordinatePoint coordinatePointA = coordinatePoints.get(0);
+        CoordinatePoint coordinatePointB = coordinatePoints.get(1);
+        return coordinatePointA.reportStraightLength(coordinatePointB);
     }
 }
--- src/test/java/coordinate/model/StraightLengthCalculatorTest.java
+++ src/test/java/coordinate/model/StraightLengthCalculatorTest.java
@@ -1,21 +1,46 @@
 package coordinate.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.offset;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class StraightLengthCalculatorTest {
 
+    CoordinatePoint coordinatePointA;
+    CoordinatePoint coordinatePointB;
+    List<CoordinatePoint> coordinatePoints = new ArrayList<>();
+
+    @BeforeEach
+    void setCoordinatePoints(){
+        coordinatePointA = new CoordinatePoint(10, 10);
+        coordinatePointB = new CoordinatePoint(2, 10);
+        coordinatePoints.add(coordinatePointA);
+        coordinatePoints.add(coordinatePointB);
+    }
+
     @Test
     void 두좌표가_서로_달아야_한다() {
-        CoordinatePoint coordinatePointA = new CoordinatePoint(2, 3);
         assertThatThrownBy(() -> new StraightLengthCalculator(List.of(coordinatePointA, coordinatePointA)))
                 .isInstanceOf(IllegalArgumentException.class).hasMessage("두 좌표가 서로 달아랴 한다.");
 
-        CoordinatePoint coordinatePointB = new CoordinatePoint(3, 2);
-        assertThatCode(() -> new StraightLengthCalculator(List.of(coordinatePointA, coordinatePointB)))
+        assertThatCode(() -> new StraightLengthCalculator(coordinatePoints))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 두좌표_사이의_직선거리를_구한다() {
+        StraightLengthCalculator straightLengthCalculatorA =
+                new StraightLengthCalculator(coordinatePoints);
+        assertThat(straightLengthCalculatorA.reportResult()).isEqualTo(8);
+
+        StraightLengthCalculator straightLengthCalculatorB =
+                new StraightLengthCalculator(List.of(coordinatePointA,new CoordinatePoint(14,15)));
+        assertThat(straightLengthCalculatorB.reportResult()).isEqualTo(6.403124,offset(0.00000099));
     }
 }
